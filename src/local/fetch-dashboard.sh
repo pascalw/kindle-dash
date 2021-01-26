986f49888f986867de577d2572846de9e86dc8d6
Pascal Widdershoven <hello@pascalw.me>
2021-01-26 20:45:27 +0100

    Update fetch-dashboard.sh to download example dashboard by default

--- src/local/fetch-dashboard.sh
+++ src/local/fetch-dashboard.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 # Fetch a new dashboard image, make sure to output it to "$1".
 # For example:
-$(dirname $0)/../ht -d -q -o "$1" get https://example.org/example.png
+$(dirname $0)/../ht -d -q -o "$1" get https://raw.githubusercontent.com/pascalw/kindle-dash/master/example/example.png
