f9c4d10e20e64b81e566bd58f315bcbeb883b176
Daniele Verducci <daniele.verducci@ichibi.eu>
2023-02-25 11:41:52 +0100

    Defaulting to xh and fallback to curl if it errs out

--- src/local/fetch-dashboard.sh
+++ src/local/fetch-dashboard.sh
@@ -5,6 +5,8 @@ CONTENT="https://raw.githubusercontent.com/pascalw/kindle-dash/master/example/ex
 
 # Fetch a new dashboard image, make sure to output it to "$1".
 # For example:
-# "$(dirname "$0")/../xh" -d -q -o "$1" get $CONTENT
-# xh seems not to be able to load certificates on Kindle PW Touch, and wget has same problem, curl should work everywhere
-curl -o "$1" "$CONTENT"
+"$(dirname "$0")/../xh" -d -q -o "$1" get $CONTENT
+# xh seems not to be able to load certificates on Kindle PW Touch: fall back to curl in case of error
+if [ $? != 0 ]; then
+  curl -o "$1" "$CONTENT"
+fi
