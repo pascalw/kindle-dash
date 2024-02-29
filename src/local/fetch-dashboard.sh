6b9d4853b52ad3d0e0384ad0e23f4ea35fc7612b
Mike Holmes <michael@mcholmes.com>
2024-02-29 16:33:14 +0000

    fetch-dashboard.sh: replaced wget with local cat of image to display, for testing

--- src/local/fetch-dashboard.sh
+++ src/local/fetch-dashboard.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 # Fetch a new dashboard image, make sure to output it to "$1".
 # For example:
-"$(dirname "$0")/../xh" -d -q -o "$1" get https://raw.githubusercontent.com/pascalw/kindle-dash/master/example/example.png
+# "$(dirname "$0")/../xh" -d -q -o "$1" get https://raw.githubusercontent.com/pascalw/kindle-dash/master/example/example.png
+cat /mnt/us/documents/dashboard.png > "$1"
